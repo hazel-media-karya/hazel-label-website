b05bde4fa0024b7fc5a773f85e77bc98f85f9dd5
hazel-media-karya <hazelmediakarya@gmail.com>
2026-07-09 12:35:24 +0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { Header } from "@/components/Header";
-import { Footer } from "@/components/Footer";
+import { Header } from "@/components/HeaderRuntime";
+import { Footer } from "@/components/FooterRuntime";
 
 export default async function LoginPage({
   searchParams,
