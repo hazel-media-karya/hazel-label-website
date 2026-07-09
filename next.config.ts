1fc835c71a470df0c5b75b8355e7efeb63cba148
hazel-media-karya <hazelmediakarya@gmail.com>
2026-07-09 22:39:08 +0000

    Revert "Use Next standalone runtime"
    This reverts commit 776028a71e3b47c93b9ae575bcca5cc9f62846d9.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   experimental: {
     staticGenerationRetryCount: 0,
     staticGenerationMaxConcurrency: 1,
